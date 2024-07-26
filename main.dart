09a34392fcdd7f74eb8228aaff643266eba11efd
bilalsalman1 <bilalsalman380@gmail.com>
2024-07-27 03:00:47 +0500

    This is list work

---
+++ main.dart
@@ -0,0 +1,25 @@
+void main() {
+  List<String> Studentname = [
+    'Ali',
+    'Ahmed',
+    'Abdullah',
+    'Sarim',
+    'Haider',
+    'Naqi',
+    'Fayz',
+  ];
+  print(Studentname);
+  List<String> Days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
+  print(Days[6]);
+  List<String> ncrgp = ['BilalSalman', 'XI', '031456', 'A', '79'];
+  print(ncrgp);
+}
+W
