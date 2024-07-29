8f9e414d64eb3b4d3b3318f37f70e5ef63bd752c
bilalsalman1 <bilalsalman380@gmail.com>
2024-07-30 03:54:39 +0500

    list work

--- main.dart
+++ main.dart
@@ -1,24 +1,51 @@
+import 'dart:math';
+
+//Question 1
+//smallest and greatest numbers
+
 void main() {
-  List<String> Studentname = [
-    'Ali',
-    'Ahmed',
-    'Abdullah',
-    'Sarim',
-    'Haider',
-    'Naqi',
-    'Fayz',
+  List<int> numbers = [27, 67, 98, 45, 09, 56, 43, 13, 26];
+  print(numbers);
+
+  int smallest = numbers.reduce(min);
+  print('smallest = ' '$smallest');
+
+  int highest = numbers.reduce(max);
+  print('highest = ' '$highest');
+
+  //Question 2
+//removing All False Values
+  List<String> usersEligibility = [
+    'John',
+    'Alice',
+    'eligible',
+    'Mike',
+    'Sarah',
+    'Tom'
   ];
-  print(Studentname);
-  List<String> Days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ];
-  print(Days[6]);
-  List<String> ncrgp = ['BilalSalman', 'XI', '031456', 'A', '79'];
-  print(ncrgp);
+  print(usersEligibility);
+  usersEligibility.removeWhere((text) => text.contains('g'));
+  print(usersEligibility);
+
+  //Question 3
+  //Returns Maximum value
+
+  List<int> number = [21, 45, 678, 200, 560, 43, 1, 577, 45, 698, 324];
+  print(number);
+  int highestnumber = number.reduce(max);
+  print('highest number = ' '$highestnumber');
+
+  //Question 4
+  //reverse a list of string
+  List<String> colors = ['Blue', 'White', 'Red', 'Purple'];
+  print(colors);
+  List<String> reversed = colors.reversed.toList();
+  print(reversed);
+
+  //Question 5
+  //filter out negative numbers
+  List<int> mixedNumbers = [3, 7, -4, 0, 4, 1, -7, -9, 6, -6];
+  print(mixedNumbers);
+  List<int> positiveNum = mixedNumbers.where((num) => num >= 0).toList();
+  print(positiveNum);
 }
